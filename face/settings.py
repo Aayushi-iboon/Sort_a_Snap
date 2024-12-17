57fbcb4ed2bddfa6e1729089b2008cef78066466
Aayushi-iboon <aayushi@iboon.io>
2024-12-17 14:54:11 +0530

    email configration web mail

--- face/settings.py
+++ face/settings.py
@@ -302,16 +302,23 @@ LOGGING = {
 
 
 # settings.py
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False 
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_NAME") 
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") 
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False 
+EMAIL_HOST = 'mail.sortasnap.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False   
+EMAIL_USE_SSL = True 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_NAME") 
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") 
-
-
-
+EMAIL_TIMEOUT = 60
 
 # jwt
 
